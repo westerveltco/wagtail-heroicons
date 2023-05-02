7fe1deb6e124b52e7c63e11baa248a01da0f6997
Reijer Boodt <3864607+ReijerTheCoder@users.noreply.github.com>
2023-05-02 23:44:09 +0200

    Explicitely convert icon Path to string

--- src/wagtail_heroicons/wagtail_hooks.py
+++ src/wagtail_heroicons/wagtail_hooks.py
@@ -11,5 +11,5 @@ from .icons import Heroicon
 @hooks.register("register_icons")
 def register_icons(icons):
     for icon in Heroicon.get_icons():
-        icons.append(icon.path)
+        icons.append(str(icon.path))
     return icons
