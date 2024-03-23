97271bb0d52b2b740ae74496f6f8f0f7a0adac11
Josh <josh@joshthomas.dev>
2024-03-23 15:23:43 -0500

    remove prints

--- tests/test_wagtail_hooks.py
+++ tests/test_wagtail_hooks.py
@@ -8,20 +8,12 @@ from wagtail import hooks
 
 def test_register_icons():
     icon_hooks = hooks.get_hooks("register_icons")
-    print("icon_hooks", icon_hooks)
-    for hook in icon_hooks:
-        print(hook.__module__)
     all_icons = sorted(itertools.chain.from_iterable(hook([]) for hook in icon_hooks))
 
     app_template_dirs = get_app_template_dirs("templates")
     template_files = [
         str(filepath) for dir_ in app_template_dirs for filepath in dir_.rglob("*")
     ]
-
-    print("all_icons")
-    print(*all_icons, sep="\n")
-    print("template_files")
-    print(*template_files, sep="\n")
 
     assert all(
         any(template_file.endswith(f"{icon}.svg") for template_file in template_files)
