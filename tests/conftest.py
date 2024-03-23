1d8d4b39c2a38c25b4bb322af12eb4ceefc2fdd1
Josh Thomas <josh@joshthomas.dev>
2024-03-23 13:25:28 -0500

    rename application template directory (#51)

---
+++ tests/conftest.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+import logging
+
+from django.conf import settings
+
+from .settings import DEFAULT_SETTINGS
+
+pytest_plugins = []  # type: ignore
+
+
+def pytest_configure(config):
+    logging.disable(logging.CRITICAL)
+
+    settings.configure(
+        **DEFAULT_SETTINGS,
+        **TEST_SETTINGS,
+    )
+
+
+TEST_SETTINGS = {
+    "INSTALLED_APPS": [
+        "wagtail_heroicons",
+        "wagtail",
+    ],
+}
